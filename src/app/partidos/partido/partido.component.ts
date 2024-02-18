89c84a61b219aafa89864b70b1fa016d8606c7c3
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-02-18 16:22:43 +0100

    inicio

---
+++ src/app/partidos/partido/partido.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-partido',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './partido.component.html',
+  styleUrl: './partido.component.css'
+})
+export class PartidoComponent {
+
+}
